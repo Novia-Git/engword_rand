78b0ad5c858a4c591ed5ccc9432b62c6e9c51c54
Novia-Git <43981539+Novia-Git@users.noreply.github.com>
2023-05-07 16:41:47 +0800

    Create randengword.py

---
+++ randengword.py
@@ -0,0 +1,28 @@
+import random
+import string
+import pandas as pd
+import time
+
+# 要產生單字的數量
+num_words = 20
+
+# 單字的長度
+word_length = 7
+
+# 產生單字
+def generate_word(words, length):
+    # 選擇num_words個隨機單字
+    return random.sample(words, length)
+
+df = pd.read_excel(r'C:\異次元\wordFrequency.xlsx',sheet_name='1 lemmas') #從第二行讀取
+worddf = df[['lemma']] 
+word_s = worddf.values.tolist() # 轉成series
+
+# 每隔15秒隨機列印單字
+while True:
+    selected_words = generate_word(word_s,num_words)
+    for word in selected_words:
+        strword = '\n'.join(word) # 轉成string
+        word = strword.strip()  # 移除單字前後的空白字元
+        print(word)
+    time.sleep(15)
