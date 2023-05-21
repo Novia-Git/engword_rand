a92d1f1a5aada769316c0e0311d6c5925a5928e2
Novia-Git <aoisora_liu@hotmail.com>
2023-05-21 21:50:50 +0800

    add link to webpage

--- randengword.py
+++ randengword.py
@@ -1,31 +1,49 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QListWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QListWidget, QLabel,QMainWindow,QListWidgetItem
 from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
+from PyQt5.QtGui import QDesktopServices, QFont
 import random
 import pandas as pd
 
-class MainWindow(QWidget):
+
+class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
 
-        # 創建界面元素
-        self.list_widget = QListWidget()
-        self.button = QPushButton('Generate Words')
+        self.initUI()
 
-        # 設置佈局
+    def initUI(self):
         layout = QVBoxLayout()
-        layout.addWidget(self.list_widget)
-        layout.addWidget(self.button)
-        self.setLayout(layout)
 
-        # 設置計時器，每15秒觸發一次產生隨機單字的功能
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.generate_words)
-        self.timer.start(15000)
+        self.labels = []
+        for _ in range(20):
+            label = QLabel(self)
+            label.setOpenExternalLinks(True)
+            layout.addWidget(label)
+            self.labels.append(label)
 
-        # 綁定按鈕事件，點擊按鈕時手動產生隨機單字
-        self.button.clicked.connect(self.generate_words)
+        self.generate_words()
 
-    # 定義函數，隨機產生10個單字
+        btn = QPushButton('Generate Words', self)
+        btn.clicked.connect(self.generate_words)
+        layout.addWidget(btn)
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+        self.setWindowTitle('Random Word Generator')
+        self.show()
+        
+
+    def openWebPage(self, link):
+        url = QUrl(link)
+        QDesktopServices.openUrl(url)
+        
+
+    # 隨機產生20個單字
     def generate_words(self):
         # 要產生單字的數量
         num_words = 20
@@ -34,22 +52,33 @@ class MainWindow(QWidget):
         worddf = df[['lemma']] # 只選擇單字的欄位
         word_s = worddf.values.tolist() # 轉成series
         selected_words = random.sample(word_s,num_words)
-        # with open('words.txt', 'r') as f:
-        #     words = f.read().splitlines()
-        # 產生10個隨機單字
+
+        # 產生20個隨機單字
         random_words =[]
         # random_words = [random.choice(selected_words) for _ in range(10)]
         for word in selected_words:
             strword = '\n'.join(word)
             random_words.append(strword)
+        
+        random.shuffle(random_words)
+        for i in range(20):
+            random_word = random_words[i]
+            label = self.labels[i]
+            # label.setText(f'<a href="http://www.example.com/{random_word}">{random_word}</a>')
+            label.setText(f'<a href="https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/{random_word}">{random_word}</a>')            
+            font = QFont()
+            font.setPointSize(14)  # 設置字體大小為16
+            label.setFont(font)
+             
         # 在界面上顯示隨機單字
-        self.list_widget.clear()
-        self.list_widget.addItems(random_words)
+        # self.list_widget.clear()
+        # self.list_widget.addItems(random_words)
        
-
+      
+    
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
-    window.resize(300, 600)
+    window.resize(400, 600)
     window.show()
     app.exec_()
